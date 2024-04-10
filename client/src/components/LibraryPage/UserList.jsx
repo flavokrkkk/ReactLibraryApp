144c0e351616baca2d5fcfe7fcdfc63b158a4840
Egor Yarovitsyn <145288995+flavokrkkk@users.noreply.github.com>
2024-04-10 15:23:23 +0300

    rename status

--- client/src/components/LibraryPage/UserList.jsx
+++ client/src/components/LibraryPage/UserList.jsx
@@ -5,94 +5,90 @@ import LibraryPageList from "./LibraryPageList";
 import LibraryPageListTwo from "./LibraryPageListTwo";
 import { useAction } from "../../store";
 
-const UserList = ({status, oneBook}) => {
+const UserList = ({ status, oneBook }) => {
+  //Получаем списки userов из store
+  const users = useSelector((state) => state.users.users);
+  const userTwo = useSelector((state) => state.usersTwo.usersTwo);
+  const {
+    addUserOneAction,
+    addUserTwoAction,
+    editStatusAction,
+    removeMyBooksAction,
+    removeUserAction,
+    removeUserTwoAction,
+    setAvailableBookAction,
+  } = useAction();
 
-    //Получаем списки userов из store
-    const users = useSelector(state => state.users.users)
-    const userTwo = useSelector(state => state.usersTwo.usersTwo)
-    const {
-            addUserOneAction,
-            addUserTwoAction,
-            editStatusAction, 
-            removeMyBooksAction, 
-            removeUserAction, 
-            removeUserTwoAction, 
-            setAvailableBookAction
-            } 
-    = useAction()
+  //Функция добавления usera в очередь за книгой
+  const addUser = (userName) => {
+    const customer = {
+      id: Date.now(),
+      name: userName,
+    };
+    addUserOneAction(customer);
+  };
 
-    //Функция добавления usera в очередь за книгой
-    const addUser = (userName) => {
-        const customer = {
-            id: Date.now(),
-            name: userName
-        }
-        addUserOneAction(customer)
+  //Функция удаления пользователя
+  const onRead = (id) => {
+    removeUserAction(id);
+  };
+
+  //Функция добавления userов которые добавили в myBook
+  const pushUserInMyBook = (users) => {
+    addUserTwoAction(users);
+  };
+
+  //Функция удаления пользователей из myBook
+  const removeUserinMyBook = (user) => {
+    removeUserTwoAction(user);
+    if (userTwo[0].id === 101) {
+      removeMyBooksAction(user.id);
     }
+  };
 
-    //Функция удаления пользователя
-    const onRead = (id) => {
-        removeUserAction(id)
-    }   
-
-    //Функция добавления userов которые добавили в myBook
-    const pushUserInMyBook = (users) => {
-        addUserTwoAction(users)
+  useEffect(() => {
+    if (oneBook.available !== userTwo.length < 3) {
+      setAvailableBookAction(userTwo.length < 3);
     }
+  }, [userTwo.length, pushUserInMyBook, removeUserinMyBook]);
 
-    //Функция удаления пользователей из myBook
-    const removeUserinMyBook = (user) => {
-            removeUserTwoAction(user)
-        if(userTwo[0].id === 101) {
-            removeMyBooksAction(user.id)
-        } 
+  useEffect(() => {
+    if (status.onHands === userTwo.length < 1) {
+      editStatusAction(!status.onHands);
     }
+  }, [userTwo.length, pushUserInMyBook, removeUserinMyBook]);
 
-    useEffect(() => {
-        if (oneBook.dostup !== userTwo.length < 3) {
-                setAvailableBookAction(userTwo.length < 3)
-        } 
-        
-    }, [userTwo.length, pushUserInMyBook, removeUserinMyBook])
+  return (
+    <div className="Libray__Page-Users">
+      <hr />
+      <h2>
+        Вы можете отслеживать очереди пользователей и читать любимые книжки!
+      </h2>
+      <div className="Library__Page-AddUser">
+        <Button onClick={() => addUser(prompt())}>Записаться в очередь</Button>
+      </div>
 
-    useEffect(() => {
-        if(status.onHands === userTwo.length < 1) {
-                editStatusAction( !status.onHands)
-        }
-    }, [userTwo.length, pushUserInMyBook, removeUserinMyBook])
-
-
-    
-    return (
-    <div className='Libray__Page-Users'>
-        <hr/>
-        <h2>Вы можете отслеживать очереди пользователей и читать любимые книжки!</h2>
-        <div className='Library__Page-AddUser'>
-            <Button onClick={() => addUser(prompt())}>Записаться в очередь</Button>
+      <div className="Library__Page-Static">
+        <div>
+          <h2>В избранном: </h2>
+          <LibraryPageList
+            users={users}
+            onRead={onRead}
+            oneBook={oneBook}
+            pushUserInMyBook={pushUserInMyBook}
+          />
         </div>
 
-        <div className='Library__Page-Static'>
-            <div>
-                <h2>В избранном: </h2>
-                <LibraryPageList
-                users={users}
-                onRead={onRead}
-                oneBook={oneBook}
-                pushUserInMyBook={pushUserInMyBook}
-                />
-            </div>
-            
-            <div>
-                <h2>В MyBooks: </h2>
-                <LibraryPageListTwo
-                userTwo={userTwo}
-                removeUserinMyBook={removeUserinMyBook}
-                />
-            </div>
+        <div>
+          <h2>В MyBooks: </h2>
+          <LibraryPageListTwo
+            userTwo={userTwo}
+            removeUserinMyBook={removeUserinMyBook}
+          />
         </div>
-       
-   </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
--- client/src/store/reducers/bookOneReducer.js
+++ client/src/store/reducers/bookOneReducer.js
@@ -1,19 +1,21 @@
-import { GET_ONE_BOOK, SET_AVAILABLE_BOOK } from "../actionCreators"
+import { GET_ONE_BOOK, SET_AVAILABLE_BOOK } from "../actionCreators";
 
 const initialState = {
-    oneBook: {
-        dostup: true,
-    },
-}
-
+  oneBook: {
+    available: true,
+  },
+};
 
 export const bookOneReducer = (state = initialState, action) => {
-        switch(action.type) {
-            case GET_ONE_BOOK: 
-                return {...state, oneBook: action.payload}
-            case SET_AVAILABLE_BOOK: 
-                return {...state, oneBook: {...state.oneBook, dostup: action.payload}}
-            default: 
-                return state
-        }
-}
+  switch (action.type) {
+    case GET_ONE_BOOK:
+      return { ...state, oneBook: action.payload };
+    case SET_AVAILABLE_BOOK:
+      return {
+        ...state,
+        oneBook: { ...state.oneBook, available: action.payload },
+      };
+    default:
+      return state;
+  }
+};
